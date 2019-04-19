13061de90993fa1016648a29f9609142a854bef3
Frank Font <frank_font@insigniafederal.com>
2019-04-19 12:52:42 -0400

    initial configuration

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# zcmd-demo-stack
-Simple demonstration stack for use with the ZCMD docker interaction framework
+PURPOSE
+=======
+Demonstrate some features of the ZCMD framework by providing a sample 
+"zcmd stack" of containers.
+
+CONVENTIONS
+===========
+The "zcmd stack" of containers is defined and managed in the "stack" folder.
+
+ # stack/stack.env <-- This is where we define our stack environment values.
+ # stack/docker-compose.yml <-- This where we define the services of our stack.
+ # stack/custom <-- By convention, we place DEV, STAGE, PROD specific confguration files into this folder.
+ 
+The runtime content of our stack services is maintained in the "runtime-shared-files" folder.
+
+AVAILABLE SERVICES IN THIS DEMO STACK
+=====================================
+Apache Webserver Demonstration Page
+ * localhost:11080
+
+Phpmyadmin -- A database management program
+ * localhost:18080
+ * Documentation available at https://github.com/phpmyadmin
+
+Adminer -- A database management program like phpmyadmin 
+ * localhost:18888
+ * Documentation available at https://docs.docker.com/samples/library/adminer/
---
+++ stack/custom/DEV/init/myinit.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+VERSIONINFO="20190419.1 DEV"
+echo "Starting $0 v$VERSIONINFO"
+
+APACHE_SERVICENAME="apache2"
+
+#ps aux | grep -q "$APACHE_SERVICENAME"
+service $APACHE_SERVICENAME status | grep -q "$APACHE_SERVICENAME is running"
+if [ $? -eq 0 ]; then =
+  echo "APACHE is running v$VERSIONINFO"
+else 
+  echo "APACHE is NOT running; starting it in 10 seconds... v$VERSIONINFO"
+  sleep 10
+  echo "APACHE is NOT running; starting it now"
+  service $APACHE_SERVICENAME start
+  sleep 10
+  service $APACHE_SERVICENAME status | grep -q "$APACHE_SERVICENAME is running"
+  if [ $? -eq 0 ]; then 
+    echo "APACHE is now running"
+  else
+    echo "APACHE STILL NOT RUNNING! Trying again..."
+    service $APACHE_SERVICENAME start
+    sleep 15
+    service $APACHE_SERVICENAME status | grep -q "$APACHE_SERVICENAME is running"
+    if [ $? -eq 0 ]; then 
+      echo "APACHE is finally running!"
+    else
+      echo "GIVING UP ON STARTING APACHE!  YOU MAY NEED TO START IT YOURSELF!"
+    fi
+  fi
+fi
+
+ps aux
+service $APACHE_SERVICENAME status
+
+#Make sure the webfiles are readable by apache
+chgrp -R www-data /var/www/html/
+
+#Place your preferred init content here
+#a2enmod rewrite
+
+/usr/bin/nohup /usr/local/bin/mailhog &
+
+echo "Now starting tail forever!"
+echo "Finished $0 v$VERSIONINFO"
+tail -f /dev/null
